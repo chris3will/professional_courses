c49098cf126babb0b26c802c7ced60a231c45cef
chris3will <13838200732@163.com>
2020-04-12 16:30:07 +0800

    完善c/s样例功能

--- 网络编程/lab_homework_week7/src/MyFTPClient.java
+++ 网络编程/lab_homework_week7/src/MyFTPClient.java
@@ -5,16 +5,22 @@ import java.net.Socket;
 
 public class MyFTPClient {
 
+    //private static boolean tryAgain = true;  //是否循环操作请求
+    public static int portNumber = 12001;
+    public static String serverIp = "localhost";
+
     class Worker implements Runnable{
         String name;
+        boolean arrived;
         Socket socket;
 
         private FileInputStream fis;
         private DataOutputStream dos;
 
-        Worker(String name, Socket socket){
+        Worker(String name, Socket socket,boolean arrived){
             this.name = name;
             this.socket = socket;
+            this.arrived = arrived;
         }
 
         //虽然是单线程，但是还是用并发方式实现
@@ -22,14 +28,17 @@ public class MyFTPClient {
         public void run() {
             try{
                 //实际上开始阶段应该先接收服务器端的一个提示
-                System.out.println("***正在与服务器连接***");
                 BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-                System.out.println("当前写入头部的哈希值为: "+ writer.hashCode() + " 已经与服务器连接成功");
-
+                if(!arrived) {  //之前没有来过
+                    System.out.println("***客户端正在与服务器连接***: " + socket.getPort());
+                    System.out.println("***当前PrintWriter的哈希值为: " + writer.hashCode() + " 已经与服务器连接成功***");
+                }
                 //下面是两条提示语
-                System.out.println("[svr]: "+reader.readLine());
-                System.out.println("[svr]: "+reader.readLine());
+                //欢迎接入本简易文件服务器，且服务器地址为: " + filepath + ". 请输入你的选择.\n 1:上传文件至终端; 2: 观察服务器信息 " +
+                //                        "3:下载终端某文件.
+                System.out.println("[svr " + socket.getPort() + "]: "+reader.readLine());
+                System.out.println("[svr " + socket.getPort() + "]: "+reader.readLine());
 
 
                 //定义一个标准输入，以对应服务器端的询问
@@ -37,73 +46,109 @@ public class MyFTPClient {
                         System.in));  //键盘的标准输入
 
                 String msg = null;  //对应标准输入的暂时缓冲内容
-                while((msg = stdIn.readLine()) != null){
+                // while(!(msg = stdIn.readLine()).equals("0")){
+                if(!(msg = stdIn.readLine()).equals("0")){
                     //向svr发送回应数据；这个while是具体的请求大循环
                     //目前下属指令均发生在情形1下，即仅有用户向客户端发出请求上传文件
 
-                    writer.println(msg);
+                      //做出你的选择
 
-                    //需要收到服务器端的具体相应，以做出接下来的判断
-                    msg = reader.readLine();
-                    System.out.println("[svr]: "+msg);
+                    if(msg.equals("1")) {
+                        writer.println(msg);
+                        //需要收到服务器端的具体相应，以做出接下来的判断
+                        msg = reader.readLine();  //"请输入您要上传文件在您电脑中的绝对路径"
+                        System.out.println("[svr " + socket.getPort() + "]: "+ msg);
 
-                    //客户输入要上传的绝对路径
-                    msg = stdIn.readLine();
-                    File file =new File(msg);
-                    if(file.exists()){
-                        //仅当文件存在时执行
-                        //我这个时候再写，是直接写到它的datainputstream里了
-                        fis = new FileInputStream(file);
-                        dos = new DataOutputStream(socket.getOutputStream());
+                        //客户输入要上传的绝对路径
+                        msg = stdIn.readLine();
+                        File file = new File(msg);
+                        if (file.exists()) {
+                            writer.println("Exists");
+                            //仅当文件存在时执行
+                            //我这个时候再写，是直接写到它的datainputstream里了
+                            fis = new FileInputStream(file);
+                            dos = new DataOutputStream(socket.getOutputStream());
 
-                        //先指明要传输的文件名和文件长度等的基本信息
-                        dos.writeUTF(file.getName());
-                        dos.flush();  //强制刷新数据流
-                        dos.writeLong(file.length());
-                        dos.flush();
-
-                        //开始传输文件
-                        System.out.println("***本地上上传启动***");
-                        byte[] bytes = new byte[1024];
-                        int length = 0;
-                        long progress = 0;  //当前进度
-                        while((length = fis.read(bytes, 0, bytes.length))!=-1){
-                            dos.write(bytes,0,length);  //从文件输入流中读取的bytes写入data输出流
+                            //先指明要传输的文件名和文件长度等的基本信息
+                            dos.writeUTF(file.getName());
+                            dos.flush();  //强制刷新数据流
+                            dos.writeLong(file.length());
                             dos.flush();
-                            progress+=length;
-                            System.out.print("| "+(100*progress / file.length())+" |\r");
+
+                            //开始传输文件
+                            System.out.println("***本地上上传启动***");
+                            byte[] bytes = new byte[1024];
+                            int length = 0;
+                            long progress = 0;  //当前进度
+
+                            long startTime = System.currentTimeMillis();
+
+                            while ((length = fis.read(bytes, 0, bytes.length)) != -1) {
+                                dos.write(bytes, 0, length);  //从文件输入流中读取的bytes写入data输出流
+                                dos.flush();
+                                progress += length;
+                                System.out.print("| " + (100 * progress / file.length()) + " |\r");
+                            }
+
+                            long endTime =System.currentTimeMillis();
+
+                            Thread.sleep(1000);
+                            System.out.println("***文件发送成功 [File Name: "+ file.getName() +" ]" +
+                                    " [time: "+(endTime-startTime)/1000 +"s ] ***");
+                            if(!socket.isClosed()) {
+                                writer.println(" 客户端正在关闭.");
+                                socket.close();  //这一步很关键，避免了一方的提前为断连式程序退出。
+                            }
+                            System.out.println("***本地文件传输成功***");
+
+                        } else {
+                            writer.println("Nop");
+                            if(!socket.isClosed()) {
+                                writer.println(" 客户端正在关闭.");
+                                socket.close();  //这一步很关键，避免了一方的提前为断连式程序退出。
+                            }
+                            System.out.println("***您好，您输入的路径非法或出现文件异常，请在本地核查后重新操作.***");
                         }
-                        Thread.sleep(1000);
-
-                        socket.close();  //这一步很关键，避免了一方的提前为断连式程序退出。
-
-                        System.out.println();
-                        //因为服务器socket或者是svr的socket提前断连了，我在这里做一个阻塞测试
-                        //下面必须接收服务器的正向反馈才能结束工作
-//                        msg = reader.readLine();
-//                        if(msg.equals("Yes"))
-//                        {
-//                            System.out.println("***本地文件传输成功***");
-//                        }else{
-//                            System.out.println("***服务器端未接收到数据，传输失败***");
-//                        }
-
-
-                        System.out.println("***本地文件传输成功***");
-
-
-                        //指明要传输的文件名和文件长度信息
-
-                    }else{
-                        //先保证不会发生意外情况
                     }
+                    else if(msg.equals("2")){
+                        writer.println(msg);
+                        //打印服务器目录文件与文件夹列表
+                        //还是通过reader来读取参数
+                        //可以先接收一个长度参数，再利用这个参数进行while循环读取每一条列表项，进而进行打印操作
 
+                        System.out.println("名\t\t||文件或文件夹\t\t||大小");
+                        msg = reader.readLine();  //先读取长度参数
+                        int len = Integer.parseInt(msg);
+                        while(len-- > 0){
+                            msg = reader.readLine();
+                            System.out.println(msg);
+                        }
 
-                    break;
+                    }
+                    else{
+                        writer.println(msg);
+                        System.out.println("***抱歉，您的操作非法或因正在维护，请重新选择***");
+                    }
                 }
 
 
-                System.out.println("***程序正常关闭中***");
+
+
+                //该次操作结束之后，询问用户是否继续使用改程序
+                System.out.println("***您是否希望继续使用本程序进行相关操作？(1/0)***");
+                msg = stdIn.readLine();
+
+                if(!socket.isClosed()) {
+                    socket.close();  //这一步很关键，避免了一方的提前为断连式程序退出。
+                }
+
+                if(!msg.equals("1")){
+                    System.out.println("***客户端线程正常关闭中***");
+                    System.out.println("***欢迎您再次使用本程序***");
+                }else{
+                    Socket Client = new Socket(serverIp,portNumber);
+                    (new Thread(new Worker((String.valueOf(Client.getPort())),Client,true))).start();
+                }
 
             }catch (Exception e){
                 e.printStackTrace();
@@ -120,19 +165,16 @@ public class MyFTPClient {
 
     public void launch() throws IOException {
         //必要的启动逻辑
-        int portNumber = 12001;
-        String serverIp = "localhost";
+
         Socket clientSocket = null;
         try{
-            clientSocket = new Socket(serverIp,portNumber);
-
-            (new Thread(new Worker("file1",clientSocket))).start();
-
+            clientSocket = new Socket(serverIp, portNumber);
+            (new Thread(new Worker((String.valueOf(clientSocket.getLocalPort())), clientSocket,false))).start();  //默认为每个用户进程开一个线程
             //(new Thread((new Worker("file2",clientSocket)))).start();
         }catch(Exception e){
             e.printStackTrace();
         }
-        //注意，我在这里不主动添加finally来让该clinet socket关闭
+        //注意，我在这里不主动添加finally来让该client socket关闭
 
     }
 
--- 网络编程/lab_homework_week7/src/MyFTPServer.java
+++ 网络编程/lab_homework_week7/src/MyFTPServer.java
@@ -3,6 +3,7 @@ import java.math.RoundingMode;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.text.DecimalFormat;
+import java.util.Date;
 import java.util.Hashtable;
 
 public class MyFTPServer {
@@ -10,6 +11,7 @@ public class MyFTPServer {
 
     //需要一个全局格式化统一的内容
     public static DecimalFormat df = null;
+    public static int portNumber = 12001;
 
     static{
         //设置数字格式
@@ -50,74 +52,94 @@ public class MyFTPServer {
                 FileOutputStream fos;  //从客户端读取文件写入
 
                 String filepath=System.getProperty("user.dir");
-                System.out.println("***有新的客户端正在接入***");
+                //System.out.println("***有新的客户端正在接入***");
                 PrintWriter writer = new PrintWriter((clientSocket.getOutputStream()),true);
-                writer.println("欢迎接入本简易文件服务器，且服务器地址为: " + filepath + ". 请输入你的选择.\n 1:上传文件至终端; 2: 观察服务器信息 " +
-                        "3:下载终端某文件.");
+                writer.println("成功接入本简易文件服务器，当前时间是 " + (new Date())+
+                        ". 服务器地址为: " + filepath + ". 请输入你的选择. \n 1:上传文件至终端; 2: 观察服务器信息; " +
+                        "3:下载终端某文件(维护中); 0: 退出本程序");
 
                 BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 String ans1 = reader.readLine();
-                System.out.println("[client]: 我要执行该编号对应的操作! "+ ans1);
+                
+                System.out.println("[client "+clientSocket.getPort()+"]: 我要执行该编号 "+ans1+ " 对应的操作! ");
 
                 if(ans1.equals("1")){
                     //要执行上传文件的基本操作
                     //询问要上传文件的绝对路径
-                    writer.println("请输入您要上传文件在您电脑中的绝对路径");
+                    writer.println("请输入您要上传文件在您电脑中的绝对路径" +
+                            "(默认忽略大小写差异，我们将遵循您本地实际的文件存储格式)");
 
-                    //等待回复,即消息一过来就要开始接收数据了
-                    dis = new DataInputStream(clientSocket.getInputStream());  //这句话不会起到阻塞的作用
+                    //如果客户端操作正确的话，就等待接收，所以我在这里再加一重检测标记
+                    String ans = reader.readLine();
+                    System.out.println("打印服务器端ans: "+ans);
+                    if(ans.equals("Exists")) {
 
-                    //获取文件头部信息
-                    String fileName = dis.readUTF();  //小心IO报错，要不然是读取不出来的
-                    long fileLength = dis.readLong();  //这两个操作和客户端都是相匹配的
-                    System.out.println("***等待用户上传数据中***");
-                    File directory = new File("user.dir");
-                    if(!directory.exists()){
-                        directory.mkdir();
+                        /* 等待回复,即消息一过来就要开始接收数据了 */
+                        dis = new DataInputStream(clientSocket.getInputStream());  //这句话不会起到阻塞的作用
+
+                        //获取文件头部信息
+                        String fileName = dis.readUTF();  //小心IO报错，要不然是读取不出来的
+                        System.out.println("***准备接收 " + fileName + " ***");
+                        long fileLength = dis.readLong();  //这两个操作和客户端都是相匹配的
+                        System.out.println("***等待客户端上传 "+fileName+" 的完整数据中***");
+                        File directory = new File("user.dir");
+                        if (!directory.exists()) {
+                            directory.mkdir();
+                        }
+
+                        //新建一个文件对象，用来存储上传的文件
+                        File file = new File(directory.getAbsolutePath() +
+                                File.separatorChar + fileName);  //处理好待写入文件的绝对路径
+
+                        fos = new FileOutputStream(file);  //定义文件输出流
+
+                        //开始接收文件余下的具体信息
+                        byte[] bytes = new byte[1024];
+                        int length = 0;
+                        long progress = 0;//进度条控制
+
+                        long startTime = System.currentTimeMillis();
+                        while ((length = dis.read(bytes, 0, bytes.length)) != -1) {
+                            fos.write(bytes, 0, length);
+                            fos.flush();
+                            //考虑到服务器的业务需求，就不显示百分比了
+//                        progress+=length;
+//                        System.out.print("| "+ (100*progress / fileLength) +" |\r");
+                        }
+                        long endTime = System.currentTimeMillis();
+                        System.out.println("***文件接收成功 [File Name: " + fileName + " ]" +
+                                " [Size: " + getFormatFileSize(fileLength) + "] [Time:" + (endTime - startTime) / 1000 + "s ]***");
+
+                        if (fos != null)
+                            fos.close();
+                        if (dis != null)
+                            dis.close();
+                    }else{
+                        //在服务器端无操作或者直接提示出错
+                        System.out.println("***客户端操作终止，服务器取消当次响应***");
                     }
 
-                    File file = new File(directory.getAbsolutePath()+
-                            File.separatorChar + fileName);  //处理好待写入文件的绝对路径
-
-                    fos = new FileOutputStream(file);  //定义文件输出流
-
-                    //开始接收文件余下的具体信息
-                    byte[] bytes = new byte[1024];
-                    int length = 0;
-                    long progress = 0 ;//进度条控制
-//                    int pos = 0;
-//                    Hashtable notate = new Hashtable();
-//                    notate.put(0,"|");
-//                    notate.put(1,"\\");
-//                    notate.put(2,"-");
-//                    notate.put(3,"/");
-                    while((length = dis.read(bytes, 0, bytes.length))!=-1){
-//                        pos++;
-                        //  | \ - / |
-                        fos.write(bytes, 0, length);
-                        fos.flush();
-                        progress+=length;
-                        System.out.print("| "+ (100*progress / fileLength) +" |\r");
-                    }
-                    System.out.println("***文件接收成功 [File Name: "+ fileName +" ]" +
-                            " [Size: "+getFormatFileSize(fileLength)+"] ***");
-
-                    //这个阻塞信息代表文件传输成功
-//                    writer.println("Yes");
-                    if(fos!=null)
-                        fos.close();
-                    if(dis!=null)
-                        dis.close();
                 }
                 if(ans1.equals("2")){
                     File f = new File(filepath);
                     String fileList[] = f.list();  //调用不带参数的list()方法
+                    writer.println(fileList.length);  //把这个列表长度参数传过去
                     for(int i=0;i<fileList.length;i++){
-                        System.out.print(fileList[i]+"\t\t||");
-                        System.out.print((new File(filepath,fileList[i]).isFile()? "文件夹"
+                        //System.out.print(fileList[i]+"\t\t||");
+                        writer.print(fileList[i]+"\t\t||");
+                        //System.out.print((new File(filepath,fileList[i]).isFile()? "文件夹"
+                        //        +"\t\t||":"文件"+"\t\t||"));
+                        writer.print((new File(filepath,fileList[i]).isFile()? "文件夹"
                                 +"\t\t||":"文件"+"\t\t||"));
-                        System.out.println((new File(filepath,fileList[i]).length() + "字节"));
+                        //System.out.println((new File(filepath,fileList[i]).length() + "字节"));
+                        writer.println((new File(filepath,fileList[i]).length() + "字节"));
                     }
+                }
+                if(ans1.equals("3")){
+
+                }
+                if(ans1.equals("0")){
+                    System.out.println("***客户端终止操作***");
                 }
 
 
@@ -126,6 +148,7 @@ public class MyFTPServer {
                 e.printStackTrace();
             }finally {
                 try {
+                    System.out.println("***已与该client "+clientSocket.getPort()+"线程断开连接***");
                     clientSocket.close();
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -141,7 +164,7 @@ public class MyFTPServer {
     }
 
     public void launch() throws IOException {
-        int portNumber = 12001;
+
         ServerSocket serverSocket = null;
         try{
             serverSocket = new ServerSocket(portNumber);
@@ -150,7 +173,8 @@ public class MyFTPServer {
             while(true){
                 //开始接收过来的client动作
                 Socket clientSocket = serverSocket.accept();
-                System.out.println("***已经与client建立新的连接***");
+                System.out.println("***已经与client "+ clientSocket.getPort()+"建立新的连接" +".接入时间为:"+(new Date())+
+                        " ***");
                 (new Thread(new Waiter(clientSocket))).start();
             }
         }catch (Exception e){
